b68609e46e4f5f41bddb4839653f6eb3890abd18
Maicon Fonseca Zanco <mzanco@br.ibm.com>
2018-11-06 00:12:34 -0200

    Create initial AddressService class

---
+++ src/main/java/com/simple_user_service/service/AddressService.java
@@ -0,0 +1,49 @@
+package com.simple_user_service.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.simple_user_service.model.Address;
+import com.simple_user_service.model.User;
+
+/**
+ * @author MaiconFonsecaZanco
+ *
+ */
+@Service
+public class AddressService {
+	private static final Logger log = LoggerFactory.getLogger(UserService.class);
+	private final JdbcTemplate jdbcTemplate;
+
+	/**
+	 * @param jdbcTemplate
+	 */
+	@Autowired
+	public AddressService(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public int save(Address address) {
+		log.info(String.format("Inserting Address: %s", address.toString()));
+		return this.jdbcTemplate.update(
+				"INSERT INTO TB_ADDRESS(POSTAL_CODE, STREET, NUMBER, DISTRICT, CITY, STATE, COUNTRY, USER_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+				address.getPostalCode(), address.getStreet(), address.getNumber(), address.getDistrict(),
+				address.getCity(), address.getState(), address.getCountry(), address.getUser().getId());
+	}
+
+	public List<Address> findAll() {
+		return jdbcTemplate.query(
+				"SELECT AD.ID, AD.POSTAL_CODE, AD.STREET, AD.NUMBER, AD.DISTRICT, AD.CITY, AD.STATE, AD.COUNTRY, U.ID AS USER_ID, U.FIRST_NAME AS USER_FIRST_NAME, U.LAST_NAME AS USER_LAST_NAME, U.CPF AS USER_CPF FROM TB_ADDRESS AD JOIN TB_USER U ON AD.USER_ID = U.ID",
+				(rs, rowNum) -> new Address(new Integer(rs.getInt("ID")), new Integer(rs.getInt("POSTAL_CODE")),
+						rs.getString("STREET"), new Integer(rs.getInt("NUMBER")), rs.getString("DISTRICT"),
+						rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"),
+						new User(new Integer(rs.getInt("USER_ID")), rs.getString("USER_FIRST_NAME"),
+								rs.getString("USER_LAST_NAME"), rs.getString("USER_CPF"))));
+	}
+
+}
