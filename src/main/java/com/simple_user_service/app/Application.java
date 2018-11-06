f193f86316c7fba8db1be306d3f5aae64de5d019
Maicon Fonseca Zanco <mzanco@br.ibm.com>
2018-11-05 22:48:35 -0200

    Add initial Application class to initialize database and list Users

---
+++ src/main/java/com/simple_user_service/app/Application.java
@@ -0,0 +1,65 @@
+package com.simple_user_service.app;
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author MaiconFonsecaZanco
+ *
+ */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.simple_user_service.model.User;
+import com.simple_user_service.service.UserService;
+
+@ComponentScan("com.simple_user_service")
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.initDB();
+		this.userService.findAll().forEach(user -> log.info(user.toString()));
+	}
+
+	/**
+	 * Initialize database with some data
+	 */
+	public void initDB() {
+		log.info("Creating tables");
+
+		jdbcTemplate.execute("DROP TABLE TB_USER IF EXISTS");
+
+		jdbcTemplate.execute(
+				"CREATE TABLE TB_USER(ID SERIAL, FIRST_NAME VARCHAR(255), LAST_NAME VARCHAR(255), CPF VARCHAR(11), ADDRESS_ID INT)");
+
+		List<User> users = new ArrayList<User>();
+		users.add(new User(null, "Enrico", "Pietro Mateus Pires", "24281154566", null));
+		users.add(new User(null, "Nicolas", "Vicente Jorge Porto", "31938839480", null));
+		users.add(new User(null, "Diogo", "Vinicius Lucas da Rocha", "31938839480", null));
+		users.forEach(user -> this.userService.save(user));
+
+	}
+}
